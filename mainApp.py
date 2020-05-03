b77328cb52ad5af9dfd48fd365763b6d3748d21b
HerculesKHL <hercules.diascampos@kidshacklabs.com>
2020-05-03 19:49:16 -0400

    Corrected indentation to match Python's
    official Python indentation is 4 spaces

--- mainApp.py
+++ mainApp.py
@@ -1,10 +1,16 @@
+'''
+    Main App file
+    Simply instantiates the Game class and runs it
+    with the config parameters specified in
+    "window_size" and "title"
+'''
 from game import Game
 
 def main():
-  window_size = (800, 450)
-  title = "Game"
-  g = Game(window_size, title)
-  g.run()
+    window_size = (800, 450)
+    title = "Game"
+    g = Game(window_size, title)
+    g.run()
 
 if __name__ == "__main__":
-  main()
+    main()
